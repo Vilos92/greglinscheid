2500bb7f25b6ab8c61e8632ee501d576cb9bbfb0
Vilos92 <linscheid.greg@gmail.com>
2022-05-08 10:52:33 -0700

    :wrench: Comment for later.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app';
 
 import GlobalStyles from '../components/GlobalStyles';
+// This provides a theme for Prism code highlighting.
 import '../styles/dracula-prism.css';
 import '../styles/globals.css';
 
