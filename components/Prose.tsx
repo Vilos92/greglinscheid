5084282142e4432bc12374f8470aaf74135ac03d
Vilos92 <linscheid.greg@gmail.com>
2022-05-10 23:07:20 -0700

    :wrench: Better inline code blocks.

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,7 +1,25 @@
 import {FC} from 'react';
+import tw, {styled} from 'twin.macro';
+
+/*
+ * Styles.
+ */
+
+const StyledProseDiv = styled.div`
+  h1:not(:first-child) {
+    margin-top: 32px;
+  }
+
+  code {
+    color: #f8f8f2;
+    background: #282a36;
+    padding: 1px 2px 1px 2px;
+    border-radius: 5px;
+  }
+`;
 
 /*
  * Component.
  */
 
-export const Prose: FC = ({children}) => <div className='prose'>{children}</div>;
+export const Prose: FC = ({children}) => <StyledProseDiv className='prose'>{children}</StyledProseDiv>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')]
 };
